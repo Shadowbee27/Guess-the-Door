f12d66271fabda4d3124d3a3ea42dbcab469b4c5
Shadowbee27 <zaun.bein@web.de>
2024-07-27 22:33:46 +0200

    Update main.rs to a stabil build

--- src/main.rs
+++ src/main.rs
@@ -1,53 +1,35 @@
 use rand::Rng;
 use std::io;
+use std::process::exit;
 
 fn main() {
-    start();
-}
-fn start() {
-    println!("Hello what is your Name?");
-    let mut name = String::new();
-    io::stdin().read_line(&mut name).expect("error");
-    println!("Hello {}", name);
-    menu();
-}
-fn menu() {
-    println!("Start the Game:s Quit:q let somebody else play:e ");
-    let mut doing = String::new();
-    io::stdin().read_line(&mut doing);
-    let mut s = String::from("s");
-    let mut q = String::from("q");
-    let mut e = String::from("e");
-    io::stdin().read_line(&mut s).expect("error");
-    io::stdin().read_line(&mut q).expect("error");
-    io::stdin().read_line(&mut e).expect("error");
-    match doing {
-        q => std::process::exit(1),
-        e => start(),
-        s => game_loop(),
-        _ => quit(),
-    }
+    game_loop();
 }
 fn game_loop() {
     let mut score: i8 = 0;
     while score < 32 {
         println!("Your score is: {}", score);
-        println!("In front of you are three doors, one of them kills you.Which one do you choose? Enter a number between 1 and 3.");
+        println!("Infront of you are 3 door, one of them kills you. Which one are you opping enter a number between 1 and 3");
         let secretnumber: i8 = rand::thread_rng().gen_range(1..=3);
         let guess = input();
         let ergebnis = vergleich(guess, secretnumber);
         if ergebnis == false {
             println!(
-                "You survived, the ghost was behind the {} door",
+                "You survived, the ghost  {}",
                 secretnumber
             );
             score = score + 1;
         } else {
             println!("You are death");
-            menu()
+            score = 0;
+            let mut idk: bool = idk();
+            if idk == true {
+            } else {
+                panic!()
+            }
         }
     }
-    println!("You won");
+    println!("Du hast gewonnen ");
 }
 fn input() -> i8 {
     let mut guess = String::new();
@@ -57,7 +39,7 @@ fn input() -> i8 {
     let guess: i8 = guess.trim().parse().expect("Please type a number!");
     return guess;
 }
-fn vergleich(guess: i8, secretnumber: i8) -> bool {
+fn compere(guess: i8, secretnumber: i8) -> bool {
     if guess == secretnumber {
         true
     } else {
@@ -65,7 +47,16 @@ fn vergleich(guess: i8, secretnumber: i8) -> bool {
     }
 }
 
+fn idk() -> bool {
+    println!("Do you want to play agian (y/n)");
+    let mut idks = String::new();
+    io::stdin().read_line(&mut idks).expect("A Error eccurt");
+    match idks {
+        y => return true,
+        n => return false,
+        _ => panic!(),
+    }
+}
 fn quit() {
-    println!("error");
     panic!()
 }
