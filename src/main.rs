3912da3aa0fdd3b0b0682aeb9d43daa06de27f0f
Shadowbee27 <zaun.bein@web.de>
2024-10-28 14:52:31 +0100

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,26 @@
+use bevy::prelude::{warn, error};
+use bevy::prelude::{Component};
 use bevy::app::{App, Startup};
 use rand::Rng;
 use std::io;
 use std::process::exit;
+use bevy::DefaultPlugins;
+use bevy::log::info;
+use bevy::utils::{error, warn};
+
+#[derive(Component)]
+pub enum GameState {
+    Startup,
+    Menu,
+    InGame,
+}
+
 fn main() {
-    App::new().add_systems(Startup, start).run();
+    App::new().add_plugins(DefaultPlugins).add_systems(Startup, start).run();
 }
 fn invalid_input(function: i8) {
     println!("Invalid Input");
+    warn!("Invalid Input");
     match function {
         1 => menu(),
         2 => store(),
@@ -14,10 +28,18 @@ fn invalid_input(function: i8) {
             println!("Please input a valid name");
             start()
         }
-        _ => panic!("A fatal Error occurred"),
+        4 => {
+            println!("Please input a valid door");
+            game_loop(0)
+        }
+        _ => {
+            error!("Invalid Input got called without a trace, this will result in a crash.");
+            panic!("A fatal Error occurred")
+        }
     }
 }
 fn start() {
+    info!("Game starts");
     println!("Hello what is your Name?");
     let mut name = String::new();
     io::stdin().read_line(&mut name).expect("error");
@@ -29,17 +51,19 @@ fn start() {
     }
 }
 fn menu() {
+    info!("Menu");
     println!("Start the Game:s Quit:q let somebody else play:e ");
     let mut doing = String::new();
     io::stdin().read_line(&mut doing).expect("An fatal error");
     match doing.trim() {
-        "q" => exit(1),
+        "q" => exit(0),
         "e" => start(),
-        "s" => game_loop(),
+        "s" => game_loop(1),
         _ => invalid_input(1),
     }
 }
-fn game_loop() {
+fn game_loop(from:i8) {
+    info!("Ingame");
     let mut score: i8 = 0;
     loop {
         println!("Your score is: {}", score);
@@ -55,7 +79,7 @@ fn game_loop() {
             score = score + 1;
         } else if score == 32 {
             println!("You won");
-            break;
+
         } else {
             println!("You are death");
             break;
@@ -68,9 +92,10 @@ fn input() -> i8 {
     io::stdin()
         .read_line(&mut guess)
         .expect("Failed to read line");
+
     let guess: i8 = guess.trim().parse().expect("Please type a number!");
-    return guess;
-}
+    return guess;}
+
 fn vergleich(guess: i8, secretnumber: i8) -> bool {
     if guess == secretnumber {
         true
