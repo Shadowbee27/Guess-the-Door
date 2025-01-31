0709b70b494bad6213304b2993c328d0c9cfb702
Shadowbee <169397554+Shadowbee27@users.noreply.github.com>
2025-01-31 13:30:38 +0100

    Updated rand crate Continued remove_access_scores.rs Added what to do System to score

--- src/gamefiles/store.rs
+++ src/gamefiles/store.rs
@@ -1,9 +1,9 @@
+use crate::lib::remove_access_scores::ScoreboardState;
 use crate::lib::{check_for_scoreboard, decode, remove_access_scores};
 use crate::menu;
 use base64::prelude::*;
 use std::fs::*;
 use std::io::Write;
-
 const SCOREBOARD_PATH: &str = "src/gamefiles/scoreboard.dat";
 pub fn store(name: String, score: i8) {
     if !check_for_scoreboard::check_if_path_exist() {
@@ -17,25 +17,51 @@ pub fn store(name: String, score: i8) {
     } else {
         println!("Found scoreboard");
     }
-    remove_access_scores::remove_scores(name.clone(), score);
     let existing_scoreboard = decode::decode();
-    let mut file = match OpenOptions::new()
-        .write(true)
-        .create(true)
-        .truncate(true)
-        .open(SCOREBOARD_PATH)
-    {
-        Ok(file) => file,
-        Err(e) => panic!("File error. {}", e),
-    };
-    let add_score: String = format!("{existing_scoreboard} \n {name} has score: {score}\n");
-    println!("Adding score...");
-    match file.write_all(BASE64_STANDARD.encode(add_score).as_bytes()) {
-        Ok(f) => {
-            println!("Added score");
-            f
+    let remove_score: (ScoreboardState, String) = remove_access_scores::remove_scores(name.clone());
+    println!("return is {}", remove_score.1);
+    if remove_score.0 == ScoreboardState::NoAdding {
+    } else if remove_score.0 == ScoreboardState::ReplaceAdding {
+        let existing_scoreboard = existing_scoreboard.replace(&remove_score.1, "");
+        println!("Replaced score");
+        let mut file = match OpenOptions::new()
+            .write(true)
+            .create(true)
+            .truncate(true)
+            .open(SCOREBOARD_PATH)
+        {
+            Ok(file) => file,
+            Err(e) => panic!("File error. {}", e),
+        };
+        let add_score: String = format!("{existing_scoreboard} \n {name} has score: {score}\n");
+        println!("Adding score...");
+        match file.write_all(BASE64_STANDARD.encode(add_score).as_bytes()) {
+            Ok(f) => {
+                println!("Added score");
+                f
+            }
+            Err(e) => panic!("Error while adding score. Exiting because: {}", e),
         }
-        Err(e) => panic!("Error while adding score. Exiting because: {}", e),
+    } else if remove_score.0 == ScoreboardState::JustAdding {
+        let existing_scoreboard = decode::decode();
+        let mut file = match OpenOptions::new()
+            .write(true)
+            .create(true)
+            .truncate(true)
+            .open(SCOREBOARD_PATH)
+        {
+            Ok(file) => file,
+            Err(e) => panic!("File error. {}", e),
+        };
+        let add_score: String = format!("{existing_scoreboard} \n {name} has score: {score}\n");
+        println!("Adding score...");
+        match file.write_all(BASE64_STANDARD.encode(add_score).as_bytes()) {
+            Ok(f) => {
+                println!("Added score");
+                f
+            }
+            Err(e) => panic!("Error while adding score. Exiting because: {}", e),
+        }
     }
     menu::menu(name)
 }
--- src/lib/remove_access_scores.rs
+++ src/lib/remove_access_scores.rs
@@ -1,17 +1,49 @@
 use crate::lib::decode;
-pub fn remove_scores(name: String, score: i8) {
+#[derive(PartialEq)]
+pub enum ScoreboardState {
+    ReplaceAdding,
+    NoAdding,
+    JustAdding,
+}
+pub fn remove_scores(name: String) -> (ScoreboardState, String) {
     let mut scores = Vec::new();
     let mut lines: Vec<String> = Vec::new();
     let scoreboard = decode::decode();
+    let mut number_scores: Vec<i8> = Vec::new();
     let string_remove = format!("{name} has score: ");
+    let mut counter = 0;
     for l in scoreboard.lines() {
-        if l.contains(&name) == true {
+        if l.contains(&name) {
             lines.push(l.to_string());
         }
-        for s in lines.clone() {
-            scores.push(s.replace(&string_remove, ""))
+    }
+    println!("Here{:?}", lines);
+    for s in lines.clone() {
+        scores.push(s.replace(&string_remove, ""));
+        counter += 1;
+    }
+    if counter < 3 || counter == 0 {
+        println!("Less than 3 scores of {name} found.");
+        (ScoreboardState::JustAdding, "".to_string())
+    } else {
+        for s in scores {
+            match s.trim().parse::<i8>() {
+                Ok(t) => number_scores.push(t),
+                Err(e) => panic!("Scoreboard has invalid data: ({e})"),
+            };
+        }
+        println!("iSIUAGLUAHDRG {:?}", number_scores);
+        let smallest = number_scores.iter().min();
+        let option_score: Vec<i8> = Vec::new();
+        if smallest == option_score.iter().min() || smallest < option_score.iter().min() {
+            (ScoreboardState::NoAdding, "".to_string())
+        } else {
+            let test = format!(" {name} has score: {}\n", smallest.unwrap());
+            println!("Z {test}");
+            (
+                ScoreboardState::ReplaceAdding,
+                format!(" {name} has score: {}\n", smallest.unwrap()),
+            )
         }
     }
-    println!("lines with name {:?}", scores);
 }
-
